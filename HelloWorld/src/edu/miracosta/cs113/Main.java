b0f04db48641728dae3571c6794e8a24342a2d5a
Spencer Thompson <spencerthompson@me.com>
2017-08-29 15:09:14 -0700

    Eclipse & Github Synergy Commit
    Integrated Eclipse with Github
    Added .gitignore file
    Setup Workspace
    Created test hellow world project

---
+++ HelloWorld/src/edu/miracosta/cs113/Main.java
@@ -0,0 +1,9 @@
+package edu.miracosta.cs113;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hellow World!");
+	}
+}
---
+++ HelloWorld/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Test Eclipse and Github with a hello world project
+ */
+/**
+ * @author Spnecer Thompson
+ */
+package edu.miracosta.cs113;
